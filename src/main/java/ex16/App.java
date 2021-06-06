6468137a5b33b1b5053842086ba506940afa102c
Zachary Flanagan <zacharyf24147@gmail.com>
2021-06-06 13:53:47 -0400

    Commit 2

--- src/main/java/ex16/App.java
+++ src/main/java/ex16/App.java
@@ -1,4 +1,21 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 16 Solution
+ *  Copyright 2021 Zachary Flanagan
+ */
+
 package ex16;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+
+        Scanner age = new Scanner(System.in);
+        System.out.print("What is your age? ");
+        String ageS = age.nextLine();
+        int ageI = Integer.parseInt(ageS);
+
+        String ternary = ageI > 15 ? "You are old enough to legally drive." : "You are not old enough to legally drive.";
+        System.out.println(ternary);
+    }
 }
